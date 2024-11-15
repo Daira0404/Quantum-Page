cda92e32c74748b15aa40bbb3e5f117ef402611e
Daira0404 <dairapenajacobo@gmail.com>
2024-11-14 23:30:31 -0500

    carrusel

--- laundry-service-website-template/quantum_page/src/App.vue
+++ laundry-service-website-template/quantum_page/src/App.vue
@@ -1,98 +1,83 @@
 <template>
   <div id="app">
-  <NavbarComponent />
-    <div id="carousel-container" class="relative">
-      <div class="w-full h-[60vh] md:h-[80vh] relative overflow-hidden">
-        <div
-          class="carousel-inner flex transition-all duration-500"
-          :style="{ transform: 'translateX(-' + currentIndex * 100 + '%)' }"
-        >
-          <div
-            v-for="(image, index) in images"
-            :key="index"
-            class="w-full h-full flex-shrink-0"
-          >
-            <img
-              class="w-full h-full object-cover"
-              :src="image.src"
-              alt="Carousel image"
-            />
-            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white">
-              <h4 class="text-xl font-semibold mb-2">{{ image.subtitle }}</h4>
-              <h1 class="text-4xl md:text-5xl font-bold mb-4">{{ image.title }}</h1>
-              <a href="#" class="btn bg-blue-500 text-white py-3 px-6 rounded-lg">
-                Learn More
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <button
-          @click="prevImage"
-          class="absolute top-1/2 left-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
-        >
-          &#10094;
-        </button>
-        <button
-          @click="nextImage"
-          class="absolute top-1/2 right-0 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full"
-        >
-          &#10095;
-        </button>
-      </div>
+    <NavbarComponent />
+    <Carousel :items-to-show="1" :autoplay-speed="3000" :wrap-around="true">
+      <slide v-for="slide in slides" :key="slide.image">
+        <img :src="slide.image" style="width: 100%; margin-top: -18px">
+        <h1 style="position: absolute; left: 90px">{{ slide.text }}</h1>
+      </slide>
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+    <div class="container-service ">
+          <div class="box-service">afaf</div>
+          <div class="box-service">aefae</div>
+          <div class="box-service">aefefa</div>
+          <div class="box-service">afeaef</div>
+          <div class="box-service">aefaef</div>
     </div>
   </div>
 </template>
 
 <script>
 import NavbarComponent from './components/NavbarComponent.vue';
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
 
 export default {
   name: 'App',
   components: {
     NavbarComponent,
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
   },
   data() {
     return {
-      currentIndex: 0,
-      images: [
+      slides: [
         {
-          src: "@/assets/carousel-1.jpg",
-          subtitle: "Laundry & Dry Cleaning",
-          title: "Best For Laundry Services",
+          image: require('@/assets/carousel-1.jpg'),  
+          text: 'Laundry Service'
         },
         {
-          src: "@/assets/carousel-2.jpg",
-          subtitle: "Laundry & Dry Cleaning",
-          title: "Highly Professional Staff",
+          image: require('@/assets/carousel-2.jpg'),  
+          text: 'Dry Cleaning Service'
         },
         {
-          src: "@/assets/carousel-3.jpg",
-          subtitle: "Laundry & Dry Cleaning",
-          title: "Fast and Reliable Service",
-        },
-      ],
-    };
-  },
-  mounted() {
-    this.carouselInterval = setInterval(this.nextImage, 5000);
-  },
-  beforeUnmount() {
-    clearInterval(this.carouselInterval);
-  },
-  methods: {
-    nextImage() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    },
-    prevImage() {
-      this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
-    },
-  },
+          image: require('@/assets/carousel-3.jpg'),  
+          text: 'Ironing Service'
+        }
+      ]
+    }
+  }
 };
 </script>
+<style>
+  .container-service {
+    position: absolute;
+    left: 10%;
+    width: 80%;
+    margin: -105px auto 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1.5px;
+    z-index: 1;
+  }
 
-<style scoped>
-.carousel-inner {
-  display: flex;
-}
+  .box-service {
+    flex: 1 1 18%;
+    height: 150px;
+    background-color: #f0f0f0;
+    transition: background-color 0.3s ease;
+    border-radius:5px;
+  }
+
+  .box-service:hover {
+    background-color: #184286;
+    height: 200px;
+    transform: translateY(-25px);
+  }
 </style>
